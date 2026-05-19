4485177c0bed2c731d931f172ab2b6670feb8e37
karlenaug <augustokarlen66@gmail.com>
2026-05-19 07:43:50 -0300

    Proyecto de HTML y CSS

---
+++ MielStore/plantilla/footer.php
@@ -0,0 +1,18 @@
+<footer class="bloques-distancia centrado p-3 mb-2 bg-warning text-dark">
+    <p>© 2025 Mielstore. Todos los derechos reservados.</p>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" href="#"><i class="fa-brands fa-instagram fa-lg" style="color: #db7500;"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-brands fa-youtube fa-lg" style="color: #ce6f03;"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true"><i class="fa-brands fa-facebook fa-lg" style="color: #bd6500;"></i></a>
+      </li>
+    </ul>
+  </footer>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+<section>
+  </body>
+</html>
